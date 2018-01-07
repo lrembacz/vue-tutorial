22225f11c726d4a2f47d69b9cdaf281369c66045
lrembacz <qwester1213@op.pl>
2018-01-07 20:52:01 +0100

    Dropping consoles via webpack mix (fix to Jeffrey change on laravel-mix)

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,5 +11,20 @@ let mix = require('laravel-mix');
  |
  */
 
+if (mix.inProduction()) {
+    mix.options({
+        uglify: {
+            uglifyOptions: {
+                warnings: false,
+                comments: false,
+                beautify: false,
+                compress: {
+                    drop_console: true,
+                }
+            }
+        }
+    });
+}
+
 mix.js('resources/assets/js/app.js', 'public/js')
-   .sass('resources/assets/sass/app.scss', 'public/css');
+    .sass('resources/assets/sass/app.scss', 'public/css');
