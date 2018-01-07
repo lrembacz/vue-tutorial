dcbf4278c9f1e39e98e9b8ec444c6f4bfaa3a538
lrembacz <qwester1213@op.pl>
2018-01-07 15:20:34 +0100

    Posts component rendering all posts passing via props from vue instance

--- resources/assets/js/components/Posts.vue
+++ resources/assets/js/components/Posts.vue
@@ -1,12 +1,20 @@
 <template>
     <div id="posts">
-        // v-for directive - rendering all posts
-        <div v-for="post in posts" class="col-md-12">
+        <div class="col-md-12">
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    <input type="text" class="form-control" v-model="filter" placeholder="Search name">
+                </div>
+            </div>
+        </div>
+         <!-- v-for directive - rendering all posts -->
+        <!-- v-for directive - rendering all filteredPosts with filter -->
+        <div v-for="post in filteredPosts" class="col-md-12">
             <div class="panel panel-default">
                 <div class="panel-heading">
                     {{ post.title }}
                     <div class="pull-right">
-                        // using of postData function to format created_at field
+                        <!-- using of postData function to format created_at field -->
                         {{ postData(post.created_at) }}
                     </div>
                 </div>
@@ -25,6 +33,11 @@
 
 <script>
     export default {
+        data() {
+            return {
+                filter : ""
+            }
+        },
         mounted() {
             console.log(this.posts)
         },
@@ -36,6 +49,14 @@
             postData: function(created) {
                 let dataTime = created.split(' ');
                 return dataTime[1] + ' | ' + dataTime[0];
+            }
+        },
+        computed: {
+            filteredPosts: function () {
+                let self = this;
+                return this.posts.filter(function (all) {
+                    return all.title.toLowerCase().indexOf(self.filter.toLowerCase()) >= 0;
+                });
             }
         }
     }
