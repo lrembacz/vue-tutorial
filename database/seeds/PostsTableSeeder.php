179b0663fa93390c20468c984f363f76997e9eea
lrembacz <qwester1213@op.pl>
2018-01-13 19:38:52 +0100

    Add Seeders

--- database/seeds/PostsTableSeeder.php
+++ database/seeds/PostsTableSeeder.php
@@ -15,24 +15,32 @@ class PostsTableSeeder extends Seeder
             'user_id' => 1,
             'title' => str_random(10),
             'content' => str_random(20),
+            'created_at' => date("Y-m-d H:i:s"),
+            'updated_at' => date("Y-m-d H:i:s")
         ]);
 
         DB::table('posts')->insert([
             'user_id' => 1,
             'title' => str_random(10),
             'content' => str_random(20),
+            'created_at' => date("Y-m-d H:i:s"),
+            'updated_at' => date("Y-m-d H:i:s")
         ]);
 
         DB::table('posts')->insert([
             'user_id' => 1,
             'title' => str_random(10),
             'content' => str_random(20),
+            'created_at' => date("Y-m-d H:i:s"),
+            'updated_at' => date("Y-m-d H:i:s")
         ]);
 
         DB::table('posts')->insert([
             'user_id' => 1,
             'title' => str_random(10),
             'content' => str_random(20),
+            'created_at' => date("Y-m-d H:i:s"),
+            'updated_at' => date("Y-m-d H:i:s")
         ]);
     }
 }
