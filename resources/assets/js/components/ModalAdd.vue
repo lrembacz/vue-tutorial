5b38491d2276d04a9165266c73914f864574408b
lrembacz <qwester1213@op.pl>
2018-01-13 20:27:16 +0100

    GetUserId from api

--- resources/assets/js/components/ModalAdd.vue
+++ resources/assets/js/components/ModalAdd.vue
@@ -52,6 +52,9 @@
                 message : ""
             }
         },
+        created() {
+            this.getUser();
+        },
         // watcher waiting for making changes on addingResponse via async call
         watch : {
             addingResponse(val) {
